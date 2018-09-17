ce126199f5577ead2a2eeca5b0f2f32a39fa310b
Mehul Bhuva <mbhuva1@cs.binghamton.edu>
2018-09-17 16:39:44 -0400

    checking www, server bind to random port and file reading function added

--- Server.py
+++ Server.py
@@ -1,28 +1,40 @@
+	
 #!/usr/bin/python
 
 import socket
 import thread
 import sys
-import datetime
-import os
-CounterList = []
+import os.path
+from os import path
 
-		
-try:
-	PORT = int(sys.argv[1]) 
-except:
-	print "Port number should be a valid integer"
+def readFile(requestedfilepath):
+	with open(requestedfilepath, "r") as filereader:
+        	filedetail = filereader.read()
+	return filedetail 
 
-
+def responseclient(client, clientaddress):
+	clientrequest = client.recv(MAX_PACKET_SIZE)
+        #print clientrequest 
+	requestmethod = clientrequest.split(' ')[0]
+        #print requestmethod
+	if requestmethod == "GET":
+		requestedfile = clientrequest.split()[1]
+		#requestedfile = requestedfile[1:].strip()
+	#print requestedfile
+		filedetail = readFile(os.getcwd() + "/www/" + requestedfile)
+               	#print filedetail 
+		client.send(filedetail)
+		client.close()
+if not path.exists('www'):
+	print "We couldn't found directory www inside your current directory please create directory www"
+	sys.exit(0)	
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Starting server on {host}:{port}".format(host=socket.gethostname().split('.')[0], port=PORT))
 try:
-        serversocket.bind(('',PORT))
-except socket.error, msg:
-        print "please enter different port number " + str(PORT) + " is already in use" 
+        serversocket.bind(('',0))
+except socket.error, msg: 
         sys.exit(0)
-print("Server on port {port} started.".format(port=PORT))
-serversocket.listen(1)
+print("Starting server on {host}:{port}".format(host=socket.gethostname().split('.')[0], port=serversocket.getsockname()[1]))
+serversocket.listen(5) #telling library that listen to 5 client in queue
 
 while 1:
 	(client, clientaddress) = serversocket.accept()
